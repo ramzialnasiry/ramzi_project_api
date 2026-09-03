9162613374287684b60118b6f2d99e4daf0f5de6
ramzialnasiry <ramzialnusayri@gmail.com>
2026-09-03 10:24:15 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,18 +4,14 @@ using ramzi_project_api.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// قاعدة البيانات
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("Dbconnection")
-    )
-);
+    ));
 
-// Identity
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddEntityFrameworkStores<AppDbContext>();
 
-// CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
@@ -26,20 +22,17 @@ builder.Services.AddCors(options =>
     });
 });
 
-// Controllers
 builder.Services.AddControllers();
 
-// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Swagger يعمل في Production أيضًا
 app.UseSwagger();
 app.UseSwaggerUI();
 
-// لا نستخدم HTTPS Redirect على Render
+// لا تستخدم HTTPS Redirection على Render
 // app.UseHttpsRedirection();
 
 app.UseAuthentication();
